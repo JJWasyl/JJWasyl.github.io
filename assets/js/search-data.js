7797adb983de4e92197ff5c76af295456e399069
JJWasyl <37805855+JJWasyl@users.noreply.github.com>
2025-06-07 18:33:02 +0000

    Deploying to gh-pages from @ JJWasyl/JJWasyl.github.io@a03bc53c0b110db8418c21928ebb2ad4cf89d5fc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,18 +30,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "post-a-post-with-plotly-js",
-        
-          title: "a post with plotly.js",
-        
-        description: "this is what included plotly.js code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2025/plotly/";
-          
-        },
-      },{id: "projects-project-1",
+        },{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
